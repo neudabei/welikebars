70fc06b7d5cc8b09965f7ce17caf750b931cbbc1
robertsj <rschultzejena@gmail.com>
2019-01-08 11:59:59 +0100

    Add some Tweet records via the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tweet.create(bar: "Cause this bar is over your head \nSo you better have arms if you're gonna pull up",
+             artist: "Eminem ft Royce Da 5'9'",
+             link_to_lyrics: 'https://genius.com/Eminem-not-alike-lyrics',
+             link_to_song: 'https://www.youtube.com/watch?v=qEGI4JjLmBA',
+             approved: true)
+
+Tweet.create(bar: "Yo yo yo",
+             artist: "Yo Man",
+             link_to_lyrics: 'https://genius.com/yoyoyo',
+             link_to_song: 'https://www.youtube.com/watch?v=qEGI4JjLmBA',
+             approved: true)
+
+Tweet.create(bar: "Crappy bar",
+             artist: "Crap man",
+             link_to_lyrics: 'https://genius.com/crappy',
+             link_to_song: 'https://www.youtube.com/watch?v=qEGI4JjLmBA',
+             approved: false)
+
+Tweet.create(bar: "Keep it real",
+             artist: "Da King",
+             link_to_lyrics: 'https://genius.com/daking-keep-it-real',
+             link_to_song: 'https://www.youtube.com/daking',
+             approved: true, 
+             tweeted_at: 1.day.ago)
+
+Tweet.create(bar: "Keep it real real",
+             artist: "Da King",
+             link_to_lyrics: 'https://genius.com/daking-keep-it-real-real',
+             link_to_song: 'https://www.youtube.com/daking2',
+             approved: true, 
+             tweeted_at: 2.day.ago)
