1d23c4dd2de8a5b061bd7a38ec77107622dbe693
robertsj <rschultzejena@gmail.com>
2019-01-20 14:40:10 +0100

    Create csv with bars and base seeds file on it

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# Testing seed data
 #
-# Examples:
+# Tweet.create(bar: "Cause this bar is over your head \nSo you better have arms if you're gonna pull up",
+#              artist: "Eminem ft Royce Da 5'9'",
+#              link_to_lyrics: 'https://genius.com/Eminem-not-alike-lyrics',
+#              link_to_song: 'https://www.youtube.com/watch?v=qEGI4JjLmBA',
+#              approved: true)
+
+# Tweet.create(bar: "Yo yo yo",
+#              artist: "Yo Man",
+#              link_to_lyrics: 'https://genius.com/yoyoyo',
+#              link_to_song: 'https://www.youtube.com/watch?v=qEGI4JjLmBA',
+#              approved: true)
+
+# Tweet.create(bar: "Crappy bar",
+#              artist: "Crap man",
+#              link_to_lyrics: 'https://genius.com/crappy',
+#              link_to_song: 'https://www.youtube.com/watch?v=qEGI4JjLmBA',
+#              approved: false)
+
+# Tweet.create(bar: "Keep it real",
+#              artist: "Da King",
+#              link_to_lyrics: 'https://genius.com/daking-keep-it-real',
+#              link_to_song: 'https://www.youtube.com/daking',
+#              approved: true, 
+#              published_at: 1.day.ago)
+
+# Tweet.create(bar: "Keep it real real",
+#              artist: "Da King",
+#              link_to_lyrics: 'https://genius.com/daking-keep-it-real-real',
+#              link_to_song: 'https://www.youtube.com/daking2',
+#              approved: true, 
+#              published_at: 2.day.ago)
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#
+require 'csv'
 
-Tweet.create(bar: "Cause this bar is over your head \nSo you better have arms if you're gonna pull up",
-             artist: "Eminem ft Royce Da 5'9'",
-             link_to_lyrics: 'https://genius.com/Eminem-not-alike-lyrics',
-             link_to_song: 'https://www.youtube.com/watch?v=qEGI4JjLmBA',
-             approved: true)
-
-Tweet.create(bar: "Yo yo yo",
-             artist: "Yo Man",
-             link_to_lyrics: 'https://genius.com/yoyoyo',
-             link_to_song: 'https://www.youtube.com/watch?v=qEGI4JjLmBA',
-             approved: true)
-
-Tweet.create(bar: "Crappy bar",
-             artist: "Crap man",
-             link_to_lyrics: 'https://genius.com/crappy',
-             link_to_song: 'https://www.youtube.com/watch?v=qEGI4JjLmBA',
-             approved: false)
-
-Tweet.create(bar: "Keep it real",
-             artist: "Da King",
-             link_to_lyrics: 'https://genius.com/daking-keep-it-real',
-             link_to_song: 'https://www.youtube.com/daking',
-             approved: true, 
-             published_at: 1.day.ago)
-
-Tweet.create(bar: "Keep it real real",
-             artist: "Da King",
-             link_to_lyrics: 'https://genius.com/daking-keep-it-real-real',
-             link_to_song: 'https://www.youtube.com/daking2',
-             approved: true, 
-             published_at: 2.day.ago)
+CSV.foreach("db/bars.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+  Tweet.create(row.to_hash)
+end
